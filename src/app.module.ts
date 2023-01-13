16b817324036ad9ded11eb6d7169ef3e1bc6d75f
Jewoo <jeus0630@gmail.com>
2023-01-14 03:32:23 +0900

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { envConfig } from './config/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(envConfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_SCHEMA,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/env.config.ts
@@ -0,0 +1,12 @@
+import { ConfigModuleOptions } from '@nestjs/config';
+
+export const envConfig: ConfigModuleOptions = {
+  envFilePath:
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : process.env.NODE_ENV === 'stage'
+      ? '.stage.env'
+      : '.development.env',
+};
+
+console.log(envConfig);

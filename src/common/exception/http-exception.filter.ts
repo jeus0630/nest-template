a0ad675b638cfa24919656866dc8e1de2dbd4c66
Jewoo <jeus0630@gmail.com>
2023-01-13 22:59:18 +0900

    Add global exception

---
+++ src/common/exception/http-exception.filter.ts
@@ -0,0 +1,39 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger();
+
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const req = ctx.getRequest<Request>();
+
+    if (!(exception instanceof HttpException)) {
+      exception = new InternalServerErrorException();
+    }
+
+    const response = (exception as HttpException).getResponse();
+
+    const stack = exception.stack;
+
+    const log = {
+      timestamp: new Date(),
+      url: req.url,
+      response,
+      stack,
+    };
+
+    this.logger.error(log);
+
+    res.status((exception as HttpException).getStatus()).json(response);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();

6bf6052068225fbf22f3fd455b4fa1c0ae5ca0b4
Jewoo <jeus0630@gmail.com>
2023-01-15 03:30:31 +0900

    Remove HMR from dependency

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 
-declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();

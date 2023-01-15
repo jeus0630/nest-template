6bb0432257821b67db39a000c8bdd3d96b5a351a
Jewoo <jeus0630@gmail.com>
2023-01-15 15:41:23 +0900

    Simplify .env configuration

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,10 +1,5 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 
 export const envConfig: ConfigModuleOptions = {
-  envFilePath:
-    process.env.NODE_ENV === 'production'
-      ? '.production.env'
-      : process.env.NODE_ENV === 'stage'
-      ? '.stage.env'
-      : '.development.env',
+  envFilePath: [`.${process.env.NODE_ENV}.env`],
 };

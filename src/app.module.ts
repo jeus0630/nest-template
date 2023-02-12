920e06c43b1e6322617579de6a8175f46fb6ceb6
Jewoo <jeus0630@gmail.com>
2023-02-12 22:13:12 +0900

    Remove unused file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { envConfig } from './config/env.config';
 import { HealthCheckController } from './health-check/health-check.controller';
@@ -13,7 +11,6 @@ import { TypeOrmConfig } from './config/tyeporm.config';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { ThrottlerConfig } from './config/throttler.config';
 import { APP_GUARD } from '@nestjs/core';
-console.log(__dirname);
 
 @Module({
   imports: [
@@ -30,8 +27,8 @@ console.log(__dirname);
     HttpModule,
     UserModule,
   ],
-  controllers: [AppController, HealthCheckController],
-  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
+  controllers: [HealthCheckController],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
